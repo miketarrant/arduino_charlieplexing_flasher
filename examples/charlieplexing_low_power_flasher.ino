786cf01f94577d2df5f12d142d500c46ec07ea30
Mike Tarrant <miketarrant@users.noreply.github.com>
2017-08-07 11:25:05 -0700

    Add files via upload
    Uploading file that was locally edited in the Arduino IDE

--- examples/charlieplexing_low_power_flasher.ino
+++ examples/charlieplexing_low_power_flasher.ino
@@ -1,142 +1,108 @@
 /**
-  
+
   Name: charlieplexing_low_power_flasher
   Purpose: Burning Man 2017 Flasher Gift Project
 
   @author Mike Tarrant
   @version  1.1   Add LDR code, comments, and code cleanup
             1.0   Initial release
-            
+
 */
 
-//#include "LowPower.h"   // RocketScream library so so we can use low-power sleep for timing delays
-#include "charlieplexing.h"  // Charlieplexing library for a six LED display
-#inclued "lowPowerSleeps.h"  // Specify low-power sleep values using the RocketScream library
+#include "charlieplexing.h"       // Charlieplexing code for a six LED display
+#include "lowPowerSleeps.h"       // Specify sleep durations and use the RocketScream low power library
 
-const int lightThreshold = 200;  // LDR values > 200 indicate it's not dark out anymore
-const int analogPinLDR = 0;  // make this a constant, so if we have to change it later, it'll be easy
+const int lightThreshold = 50;    // LDR values above the lightThreshold indicate it's not dark out anymore
+const int analogPinLDR = 0;       // analog input pin for the light-dependent resistor (LDR)
+const int digitalPinLDR = 8;      // digital output pin to power the LDR circuit when we need to check the light levels
+
+//int howMuchLight = 300;
 
 void setup()
 {
-  pinMode(PIN_A, OUTPUT);  //  set initial state of charlieplexed display
-  pinMode(PIN_B, OUTPUT); 
-  pinMode(PIN_C, OUTPUT);  
+  pinMode(PIN_A, OUTPUT);         //  set initial state of charlieplexed display
+  pinMode(PIN_B, OUTPUT);
+  pinMode(PIN_C, OUTPUT);
 }
 
 
-
-void loop()   // Let's do this thing, flash if it's dark out; low-power waiting if it's light out
+void loop()   // Let's do this thing. Flash if it's dark out; low-power waiting if it's light out
 {
-    
-  //for(;;)   // this might be redundant with the void loop() above -- test later when I'm not waiting at the airport...
-  
-  while(analogRead(analogPinLDR) < lightThreshold)  // if the LDR value is < lightThreshold, it's night so we flash the LEDs
-  {
+
+  while (measureLight() < lightThreshold) // if the light value is < lightThreshold, it's night so we flash the LEDs
+  {    
     setLED(1);              // turn LED 1 on
     setLowPwrSleep(60);     // low-pwr sleep for 60mS
-    //delay(65);
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    
-    setLED(2);              // turn LED 2 on 
+
+    setLED(2);              // turn LED 2 on
     setLowPwrSleep(90);     // low-pwr sleep for 90mS
-    //delay(85);
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    
+
     setLED(3);              // turn LED 3 on
     setLowPwrSleep(120);    // low-pwr sleep for 120mS
-    //delay(95);
-    //LowPower.idle(SLEEP_120MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    
-    setLED(4);              // turn LED 4 on; 
-    setLowPwrSleep(90);     //low-pwr sleep for 90mS
-    //delay(85);
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    
-    setLED(5);              // turn LED 5 on
-    setLowPwrSleep(60);     //low-pwr sleep for 60mS
-    //delay(65);
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF); 
-    
-    setLED(6);              // turn LED 6 on
-    setLowPwrSleep(45);     //low-pwr sleep for 45mS
-    //delay(45);
-    //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    //LowPower.idle(SLEEP_15MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);   
+
+    setLED(4);              // turn LED 4 on;
+    setLowPwrSleep(90);     // low-pwr sleep for 90mS
 
     setLED(5);              // turn LED 5 on
-    setLowPwrSleep(30);     //low-pwr sleep for 30mS
-    //delay(35);
-    //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);   
-  
+    setLowPwrSleep(60);     // low-pwr sleep for 60mS
+
+    setLED(6);              // turn LED 6 on
+    setLowPwrSleep(45);     // low-pwr sleep for 45mS
+
+    setLED(5);              // turn LED 5 on
+    setLowPwrSleep(30);     // low-pwr sleep for 30mS
+
     setLED(6);              // turn LED 6 on
     setLowPwrSleep(45);     //low-pwr sleep for 45mS
-    //delay(45);
-    //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    //LowPower.idle(SLEEP_15MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    
-  
+
     setLED(5);              // turn LED 5 on
     setLowPwrSleep(60);     //low-pwr sleep for 60mS
-    //delay(65);
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);    
-  
+
     setLED(4);              // turn LED 4 on
-    setLowPwrSleep(90);     //low-pwr sleep for 90mS
-    //delay(85);
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    
+    setLowPwrSleep(90);     // low-pwr sleep for 90mS
+
     setLED(3);              // turn LED 3 on
-    setLowPwrSleep(90);     //low-pwr sleep for 90mS
-    //delay(95); 
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-       
+    setLowPwrSleep(90);     // low-pwr sleep for 90mS
+
     setLED(2);              // turn LED 2 on
-    setLowPwrSleep(90);     //low-pwr sleep for 90mS
-    //delay(85); 
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-    
+    setLowPwrSleep(90);     // low-pwr sleep for 90mS
+
     setLED(1);              // turn LED 1 on
     setLowPwrSleep(60);     //low-pwr sleep for 60mS
-    //delay(65); 
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
 
     setLED(2);              // turn LED 2 on
     setLowPwrSleep(60);     //low-pwr sleep for 60mS
-    //delay(50); 
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-  
-    setLED(1);              // turn LED 1 on
-    setLowPwrSleep(30);     //low-pwr sleep for 30mS
-    //delay(35); 
-    //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-
-    setLED(2);              // turn LED 2 on
-    setLowPwrSleep(15);     //low-pwr sleep for 15mS
-    //delay(20); 
-    //LowPower.idle(SLEEP_15MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
 
     setLED(1);              // turn LED 1 on
-    setLowPwrSleep(30);     //low-pwr sleep for 30mS
-    //delay(35);
-    //LowPower.idle(SLEEP_30MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF); 
+    setLowPwrSleep(30);     // low-pwr sleep for 30mS
 
     setLED(2);              // turn LED 2 on
-    setLowPwrSleep(60);     //low-pwr sleep for 60mS
-    //delay(50); 
-    //LowPower.idle(SLEEP_60MS, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
+    setLowPwrSleep(15);     // low-pwr sleep for 15mS
+
+    setLED(1);              // turn LED 1 on
+    setLowPwrSleep(30);     // low-pwr sleep for 30mS
+
+    setLED(2);              // turn LED 2 on
+    setLowPwrSleep(60);     // low-pwr sleep for 60mS
 
     setLED(0);              // turn all the LEDs off
-    setLowPwrSleep(1000);   //low-power sleep for 1000 mS, end of flash cycle
-    //LowPower.idle(SLEEP_1S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
-              
-  }   // end of while(), do another flash cycle if the LDR says it's still dark out... 
-      
-  setLowPwrSleep(8000);   // ...if the LDR says it's now light out, nap 8 seconds and check it again
-  //LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_OFF, TIMER1_OFF, TIMER0_OFF, SPI_OFF, USART0_OFF, TWI_OFF);
+    setLowPwrSleep(1000);   // low-power sleep for 1000 mS, end of flash cycle
 
-}  // end of the void loop(), go back and do it again...
+  }   // end of while(), do another flash cycle if the LDR says it's still dark out...
+
+  //digitalWrite(13, HIGH);              // turn the onboard LED ON for troubleshooting
+  setLowPwrSleep(8000);     // ...if the LDR says it's light out now, nap 8000 mS, then check it again
+  //digitalWrite(13, LOW);              // turn the onboard LED OFF for troubleshooting
+
+
+}     // end of the void loop(), go back and do it again.
+
+
+int measureLight()
+  {
+    digitalWrite(digitalPinLDR, HIGH);              // turn on the LDR curcuit
+    int howMuchLight = analogRead(analogPinLDR);    // read the light value from the LDR circuit
+    digitalWrite(digitalPinLDR, LOW);               // turn off the LDR circuit      
+    return howMuchLight;
+  }
+
